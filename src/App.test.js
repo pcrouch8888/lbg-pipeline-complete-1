c77e9ea43d997be37a0b386f4f815ff0fd6f2bef
peter crouch <peter.crouch2@virgin.net>
2023-03-09 17:14:54 +0000

    did something sweet

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders VAT Calculator text', () => {
+test('renders PETES VAT Calculator text', () => {
   render(<App />);
   const heading = screen.getByText(/VAT CALCULATOR/i);
   expect(heading).toBeInTheDocument();
